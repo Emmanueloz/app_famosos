ee4c17203f02193569ba1889789a78f926b49790
Emmanueloz <davidozuna84@hotmail.com>
2023-11-15 12:45:19 -0600

    formulario de agregar famosos

--- lib/src/page/form_famoso.dart
+++ lib/src/page/form_famoso.dart
@@ -10,50 +10,63 @@ class FormFamososPage extends StatelessWidget {
         title: const Text('Agregar Famoso'),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10),
-          child: const Column(children: <Widget>[
-            TextField(
-              keyboardType: TextInputType.name,
-              decoration: InputDecoration(
-                labelText: "Nombre",
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Edad",
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Lugar de origen",
-              ),
-            ),
-            TextField(
-              keyboardType: TextInputType.datetime,
-              decoration: InputDecoration(
-                labelText: "Fecha de nacimiento",
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Tipo de famoso",
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Genero",
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Pareja",
-              ),
-            )
-          ]),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            _buildTextField("Nombre", TextInputType.name),
+            _buildTextField("Edad", TextInputType.number),
+            _buildTextField("Lugar de origen", TextInputType.text),
+            _buildTextField("Fecha de nacimiento", TextInputType.datetime),
+            _buildTextField("Tipo de famoso", TextInputType.text),
+            _buildGenderRadioButtons(),
+            _buildTextField("Pareja", null),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField(String labelText, TextInputType? keyboardType) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextField(
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          labelText: labelText,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildGenderRadioButtons() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          "Género",
+          style: TextStyle(fontSize: 16),
+        ),
+        Row(
+          children: <Widget>[
+            Radio<String>(
+              value: "M",
+              groupValue: "gender",
+              onChanged: (String? value) {
+                print(value);
+              },
+            ),
+            const Text("Masculino"),
+            Radio<String>(
+              value: "F",
+              groupValue: "gender",
+              onChanged: (String? value) {
+                print(value);
+              },
+            ),
+            const Text("Femenino"),
+          ],
+        ),
+      ],
     );
   }
 }
