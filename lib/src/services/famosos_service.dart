4276033c8bd16eea57ec70c20be72010b99229fc
Emmanueloz <davidozuna84@hotmail.com>
2023-11-15 15:47:57 -0600

    url de la base de datos a firebase

--- lib/src/services/famosos_service.dart
+++ lib/src/services/famosos_service.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-class FamososService extends ChangeNotifier {}
+class FamososService extends ChangeNotifier {
+  final String _baseUrl =
+      "https://bdra3-5b065-default-rtdb.firebaseio.com"; // famosos.json
+}
--- lib/src/services/peliculas_service.dart
+++ lib/src/services/peliculas_service.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
-class PeliculasService extends ChangeNotifier {}
+class PeliculasService extends ChangeNotifier {
+  final String _baseUrl = "https://bdra3-5b065-default-rtdb.firebaseio.com";
+}
