b275e5d587fa6125e7fcfaf7646a32e340744455
Alexis1212asaxx <felixalexissanchezlopez13@gmail.com>
2023-11-15 18:52:53 -0600

    Parte 1_Peliculas

--- lib/src/services/peliculas_service.dart
+++ lib/src/services/peliculas_service.dart
@@ -1,5 +1,36 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:app_famosos/src/models/pelicula_modelo.dart';
 import 'package:flutter/material.dart';
 
-class PeliculasService extends ChangeNotifier {
-  final String _baseUrl = "https://bdra3-5b065-default-rtdb.firebaseio.com";
+  class PeliculaService extends ChangeNotifier{
+  final String _baseUrl = 'hprod-10d9a-default-rtdb.firebaseio.com';
+  final List<PeliculaModelo> pelicula = [];
+  late PeliculaModelo selectedPelicula;
+
+  Future<String?> createPelicula(PeliculaModelo pelicula) async {
+    final url = Uri.https(_baseUrl,'peliculas.json');
+    final resp = await http.post(url, body: json.encode(pelicula));
+    final decodedData = json.decode(resp.body);
+
+    pelicula.id = decodedData['name'];
+    return pelicula.id;
+  } 
+
+  //leer los  datos de firebase
+  Future<List<PeliculaModelo>> loadPeliculas() async {
+    final List<PeliculaModelo> pelicula = [];
+    final url = Uri.http(_baseUrl, 'peliculas.json');
+
+    final resp = await http.get(url); // Make the HTTP request
+    
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
+
+    productsMap.forEach((key, value) {
+      final tempPeliculas = PeliculaModelo.fromJson(value);
+      tempPeliculas.id = key;
+      pelicula.add(tempPeliculas);
+    });
+    return pelicula;
+  } 
 }
