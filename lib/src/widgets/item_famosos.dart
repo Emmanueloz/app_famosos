02773cf7f42e41e4458a48806718ae9777acd8ea
Wyvern-Knight <mraul7875@gmail.com>
2023-11-17 13:58:21 -0600

    agregado de tipo y color segun el genero

--- lib/src/widgets/item_famosos.dart
+++ lib/src/widgets/item_famosos.dart
@@ -1,4 +1,3 @@
-
 import 'package:app_famosos/src/models/famoso_modelo.dart';
 
 import 'package:flutter/material.dart';
@@ -10,14 +9,25 @@ class ItemFamosos extends StatelessWidget {
     required this.famosos,
   });
 
-
   @override
   Widget build(BuildContext context) {
+    Color tipoColor = Colors.black; 
+
+    
+    if (famosos.genero == "M") {
+      tipoColor = Colors.blue;
+    } else if (famosos.genero == "F") {
+      tipoColor = Colors.pink;
+    }
+
     return Card(
       child: ListTile(
-        title:  Text(famosos.nombre),
+        title: Text(famosos.nombre),
+        subtitle: Text(
+          famosos.tipo,
+          style: TextStyle(color: tipoColor),
+        ),
       ),
     );
   }
 }
-
